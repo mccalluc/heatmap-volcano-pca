fdf621a4034748b18d0e9c643deca99e8c77e871
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-05 19:00:14 -0500

    cleanup: test-main in a more functional style. passes locally

--- test/test-main.js
+++ test/test-main.js
@@ -1,23 +1,18 @@
-var tests = [];
-
-Object.keys(window.__karma__.files).forEach(function (file) {
-  if (/(spec|test)\.js$/i.test(file)) {
-    var normalized = file.replace(/\.js$/g, '');
-    tests.push(normalized);
-  }
-});
-
 require.config({
-  // Karma serves files under /base, which is the basePath from your config file
+  // Karma serves files under /base, and our default is src
   baseUrl: '/base/src',
 
   paths: {
-    test: '../test',
+    test: '../test', // ie, up and over from src
     d3: "https://cdnjs.cloudflare.com/ajax/libs/d3/4.4.0/d3.min"
   },
 
   // dynamically load all test files
-  deps: tests,
+  deps: Object.keys(window.__karma__.files).filter(function(file) {
+    return file.match(/test\.js$/);
+  }).map(function(file) {
+    return file.replace(/\.js$/g, '');
+  }),
 
   // start test run, once Require.js is done
   callback: window.__karma__.start
