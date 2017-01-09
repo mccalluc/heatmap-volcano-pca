78d6187e8b783e5da951c8c8dc8068dcced44f91
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-09 10:29:02 -0500

    readability

--- test/heatmap_scatterplot_test.js
+++ test/heatmap_scatterplot_test.js
@@ -9,8 +9,8 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
 
   describe('heatmap_axes', function () {
     it('works', function () {
-      var matrix = [[1,2],[3,4]];
-      matrix.columns = ['a','b'];
+      var matrix = [[1, 2], [3, 4]];
+      matrix.columns = ['id', 'col-1'];
       var vis = d3
           .selectAll('body')
           .append('div')
@@ -20,7 +20,14 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
       expect(vis.size()).toEqual(1);
       var svg = d3.select('svg');
       expect(svg.attr('height')).toEqual('50');
-      expect(svg.node().innerHTML).toEqual('<g transform="translate(0,50)"><g text-anchor="end" font-family="sans-serif" font-size="10" fill="none"><g transform="translate(0,0)" opacity="1" class="tick"><text style="transform: rotate(90deg); font-weight: bold; cursor: default;" dy="0em" x="0.5" y="-9" fill="#000">b</text></g></g></g>');
+      expect(svg.node().innerHTML).toEqual(
+          '<g transform="translate(0,50)">' +
+          '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
+          '<g transform="translate(0,0)" opacity="1" class="tick">' +
+          '<text style="' +
+          'transform: rotate(90deg); font-weight: bold; cursor: default;' +
+          '" dy="0em" x="0.5" y="-9" fill="#000">col-1</text>' +
+          '</g></g></g>');
     });
   });
 });
