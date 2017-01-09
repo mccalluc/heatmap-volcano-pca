300ecd66f46128e4ddab5596383f71b3a470e9a6
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-09 11:38:13 -0500

    Helper functions for html tests

--- test/heatmap_scatterplot_test.js
+++ test/heatmap_scatterplot_test.js
@@ -1,9 +1,9 @@
 define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
 
   describe('heatmap_scatterplot', function () {
-var internals = chart().__internals__;
+    var internals = chart().__internals__;
 
-    beforeEach(function() {
+    beforeEach(function () {
       d3.selectAll('body *').remove()
     });
 
@@ -14,6 +14,30 @@ var internals = chart().__internals__;
     });
 
     describe('heatmap_axes', function () {
+      function gg(content) {
+        return '<g transform="translate(0,50)">' +
+            '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
+            content +
+            '</g></g>';
+      }
+
+      function translate(t, content) {
+        return '<g transform="translate(' + t + ',0)" opacity="1" class="tick">' + content + '</g>';
+      }
+
+      function default_text(content) {
+        return '<text style="' +
+            'transform: rotate(90deg); font-weight: bold; cursor: default;' +
+            '" dy="0em" x="0.5" y="-9" fill="#000">' + content + '</text>';
+      }
+
+      function pointer_text(content) {
+        return '<text style="' +
+            'transform: rotate(90deg); cursor: pointer;' +
+            '" dy="0em" x="0.5" y="-9" fill="#000">' + content +
+            '<title>alt-click to change x-axis</title></text>';
+      }
+
       it('works for 1 column degenerate case', function () {
         var matrix = [[1, 2], [3, 4]];
         matrix.columns = ['id', 'col-1'];
@@ -27,18 +51,13 @@ var internals = chart().__internals__;
         var svg = d3.select('svg');
         expect(svg.attr('height')).toEqual('50');
         expect(svg.node().innerHTML).toEqual(
-            '<g transform="translate(0,50)">' +
-            '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
-            '<g transform="translate(0,0)" opacity="1" class="tick">' +
-            '<text style="' +
-            'transform: rotate(90deg); font-weight: bold; cursor: default;' +
-            '" dy="0em" x="0.5" y="-9" fill="#000">col-1</text>' +
-            '</g></g></g>');
+            gg(translate(0, default_text('col-1')))
+        );
       });
 
       it('works for 3 column realistic case', function () {
         var matrix = [[0, 1, 2, 3]];
-        matrix.columns = ['id', 'col-X', 'col-2', 'col-3', 'col-4'];
+        matrix.columns = ['id', 'col-1', 'col-2', 'col-3', 'col-4'];
         var vis = d3
             .selectAll('body')
             .append('div')
@@ -48,14 +67,14 @@ var internals = chart().__internals__;
         expect(vis.size()).toEqual(1);
         var svg = d3.select('svg');
         expect(svg.attr('height')).toEqual('50');
-        // expect(svg.node().innerHTML).toEqual(
-        //     '<g transform="translate(0,50)">' +
-        //     '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
-        //     '<g transform="translate(0,0)" opacity="1" class="tick">' +
-        //     '<text style="' +
-        //     'transform: rotate(90deg); font-weight: bold; cursor: default;' +
-        //     '" dy="0em" x="0.5" y="-9" fill="#000">col-1</text>' +
-        //     '</g></g></g>');
+        expect(svg.node().innerHTML).toEqual(
+            gg(
+                translate(0, default_text('col-1'))
+                + translate(75, default_text('col-2'))
+                + translate(150, pointer_text('col-3'))
+                + translate(225, pointer_text('col-4'))
+            )
+        );
       });
     });
   });
