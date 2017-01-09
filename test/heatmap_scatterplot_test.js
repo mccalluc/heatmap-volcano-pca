d7f52c9009891e0212e6e7957ed713a4bae18365
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-09 15:55:12 -0500

    ugly test for scatterplot axes

--- test/heatmap_scatterplot_test.js
+++ test/heatmap_scatterplot_test.js
@@ -45,7 +45,7 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
         }
 
         it('works for 1 column degenerate case', function () {
-          var matrix = [[1, 2], [3, 4]];
+          var matrix = ['not used here'];
           matrix.columns = ['id', 'col-1'];
           var vis = d3
               .selectAll('body')
@@ -60,7 +60,7 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
         });
 
         it('works for 3 column realistic case', function () {
-          var matrix = ['not used by this method'];
+          var matrix = ['not used here'];
           matrix.columns = ['id', 'col-1', 'col-2', 'col-3', 'col-4'];
           var vis = d3
               .selectAll('body')
@@ -108,12 +108,61 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
           expect(canvas(vis).width).toEqual(3);
           expect(canvas(vis).height).toEqual(1);
 
-          expect(pixel(vis, 0, 0)).toEqual([128, 128, 255, 255]); // blue
+          expect(pixel(vis, 0, 0)).toEqual([128, 128, 255, 255]); // pale blue
           expect(pixel(vis, 1, 0)).toEqual([255, 255, 255, 255]); // white
           expect(pixel(vis, 2, 0)).toEqual([255, 0, 0, 255]); // red
         });
       });
     });
+
+    describe('scatterplot', function () {
+      describe('scatterplot_axes', function () {
+        function handle(a, b) {
+          return '<rect style="display: none;" cursor="' + a + '-resize" ' +
+              'class="handle handle--' + b + '"></rect>';
+        }
+
+        it('works', function () {
+          var matrix = [{id: 42, neg: -1, zero: 0, pos: 2}];
+          matrix.columns = ['id', 'neg', 'zero', 'pos'];
+          var vis = d3
+              .selectAll('body')
+              .append('div')
+              .data([matrix])
+              .call(internals.scatterplot_axes);
+
+          expect(vis.size()).toEqual(1);
+          expect(vis.select('svg').node().innerHTML).toEqual(
+              '<g transform="translate(50,50)">' +
+              '<g text-anchor="middle" font-family="sans-serif" font-size="10" fill="none" transform="translate(0,300)">' +
+              '<path d="M0.5,6V0.5H300.5V6" stroke="#000" class="domain">' +
+              '</path></g>' +
+              '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
+              '<path d="M-6,300.5H0.5V0.5H-6" stroke="#000" class="domain">' +
+              '</path></g>' +
+              '<text style="text-anchor: middle; cursor: default;" transform="translate(150,337.5)">neg</text>' +
+              '<text style="text-anchor: middle; cursor: default;" transform="translate(-37.5,150) rotate(90)">zero</text>' +
+              '<g pointer-events="all" fill="none" class="brush">' +
+              '<rect height="300" width="300" y="0" x="0" cursor="crosshair" pointer-events="all" class="overlay"></rect>' +
+              '<rect style="display: none;" shape-rendering="crispEdges" stroke="#fff" fill-opacity="0.3" fill="#777" cursor="move" class="selection"></rect>' +
+              handle('ns', 'n') +
+              handle('ew', 'e') +
+              handle('ns', 's') +
+              handle('ew', 'w') +
+              handle('nwse', 'nw') +
+              handle('nesw', 'ne') +
+              handle('nwse', 'se') +
+              handle('nesw', 'sw') +
+              '</g></g>'
+          );
+        });
+
+      });
+      describe('scatterplot_body', function () {
+//TODO
+      });
+
+    })
 
   });
 
