cd33ad2a1e09da59dd4be57dbbab819b61162b99
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-01-10 14:49:18 -0500

    Convert xml to JSON for more robust comparisons

--- test/heatmap_scatterplot_test.js
+++ test/heatmap_scatterplot_test.js
@@ -1,4 +1,4 @@
-define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
+define(['heatmap_scatterplot', 'd3', '../test/utils'], function (chart, d3, utils) {
 
   describe('heatmap_scatterplot', function () {
     var internals = chart().__internals__;
@@ -36,7 +36,7 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
       describe('heatmap_axes', function () {
         function gg(content) {
           return '<g transform="translate(0,50)">' +
-              '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
+              '<g font-family="sans-serif" font-size="10" fill="none" text-anchor="end">' +
               content +
               '</g></g>';
         }
@@ -67,8 +67,8 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
               .call(internals.heatmap_axes);
 
           expect(vis.size()).toEqual(1);
-          expect(vis.select('svg').node().innerHTML).toEqual(
-              gg(translate(0, default_text('col-1')))
+          expect(utils.xml_node_to_json(vis.select('svg').node())).toEqual(
+              utils.xml_string_to_json(gg(translate(0, default_text('col-1'))))
           );
         });
 
@@ -81,13 +81,13 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
               .call(internals.heatmap_axes);
 
           expect(vis.size()).toEqual(1);
-          expect(vis.select('svg').node().innerHTML).toEqual(
-              gg(
+          expect(utils.xml_node_to_json(vis.select('svg').node())).toEqual(
+              utils.xml_string_to_json(gg(
                   translate(0, default_text('col-1'))
                   + translate(75, default_text('col-2'))
                   + translate(150, pointer_text('col-3'))
                   + translate(225, pointer_text('col-4'))
-              )
+              ))
           );
         });
       });
@@ -130,28 +130,30 @@ define(['heatmap_scatterplot', 'd3'], function (chart, d3) {
               .call(internals.scatterplot_axes);
 
           expect(vis.size()).toEqual(1);
-          expect(vis.select('svg').node().innerHTML).toEqual(
-              '<g transform="translate(50,50)">' +
-              '<g text-anchor="middle" font-family="sans-serif" font-size="10" fill="none" transform="translate(0,300)">' +
-              '<path d="M0.5,6V0.5H300.5V6" stroke="#000" class="domain">' +
-              '</path></g>' +
-              '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
-              '<path d="M-6,300.5H0.5V0.5H-6" stroke="#000" class="domain">' +
-              '</path></g>' +
-              '<text style="text-anchor: middle; cursor: default;" transform="translate(150,337.5)">neg</text>' +
-              '<text style="text-anchor: middle; cursor: default;" transform="translate(-37.5,150) rotate(90)">zero</text>' +
-              '<g pointer-events="all" fill="none" class="brush">' +
-              '<rect height="300" width="300" y="0" x="0" cursor="crosshair" pointer-events="all" class="overlay"></rect>' +
-              '<rect style="display: none;" shape-rendering="crispEdges" stroke="#fff" fill-opacity="0.3" fill="#777" cursor="move" class="selection"></rect>' +
-              handle('ns', 'n') +
-              handle('ew', 'e') +
-              handle('ns', 's') +
-              handle('ew', 'w') +
-              handle('nwse', 'nw') +
-              handle('nesw', 'ne') +
-              handle('nwse', 'se') +
-              handle('nesw', 'sw') +
-              '</g></g>'
+          expect(utils.xml_node_to_json(vis.select('svg').node())).toEqual(
+              utils.xml_string_to_json(
+                  '<g transform="translate(50,50)">' +
+                  '<g text-anchor="middle" font-family="sans-serif" font-size="10" fill="none" transform="translate(0,300)">' +
+                  '<path d="M0.5,6V0.5H300.5V6" stroke="#000" class="domain">' +
+                  '</path></g>' +
+                  '<g text-anchor="end" font-family="sans-serif" font-size="10" fill="none">' +
+                  '<path d="M-6,300.5H0.5V0.5H-6" stroke="#000" class="domain">' +
+                  '</path></g>' +
+                  '<text style="text-anchor: middle; cursor: default;" transform="translate(150,337.5)">neg</text>' +
+                  '<text style="text-anchor: middle; cursor: default;" transform="translate(-37.5,150) rotate(90)">zero</text>' +
+                  '<g pointer-events="all" fill="none" class="brush">' +
+                  '<rect height="300" width="300" y="0" x="0" cursor="crosshair" pointer-events="all" class="overlay"></rect>' +
+                  '<rect style="display: none;" shape-rendering="crispEdges" stroke="#fff" fill-opacity="0.3" fill="#777" cursor="move" class="selection"></rect>' +
+                  handle('ns', 'n') +
+                  handle('ew', 'e') +
+                  handle('ns', 's') +
+                  handle('ew', 'w') +
+                  handle('nwse', 'nw') +
+                  handle('nesw', 'ne') +
+                  handle('nwse', 'se') +
+                  handle('nesw', 'sw') +
+                  '</g></g>'
+              )
           );
         });
 
